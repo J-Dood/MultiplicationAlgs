3bfa5902dae624dfa7184a24395b5d93ed8464a7
Dood <doodwheresmycar@gmail.com>
2022-02-06 20:23:19 -0700

    Started on the Polynomial object and the main methods for data collection.

--- Polynomial.py
+++ Polynomial.py
@@ -5,3 +5,30 @@
 #
 # This file is the array based polynomial object
 
+
+# The class stores the coefficients of a polynomial in an array where the index corresponds
+# to the degree of the term.
+class Polynomial:
+
+# Constructor
+    def __init__(self, coefficients):
+        self.coefficients = coefficients
+
+
+# This function returns the maximum term of a polynomial as a string
+    def getmaxTerm(self):
+        return str(self.coefficients[self.coefficients.length]) + "X^" + str(self.coefficients.length)
+
+
+# This function returns the minimum term of a polynomial as a string
+    def getminTerm(self):
+        return str(self.coefficients[0])
+
+
+# This function returns all of the terms of a polynomial as a string
+    def getall(self):
+        polynomial = "Y(X) ="
+        for i in range(self.coefficients.length):
+            polynomial += (" " + str() + "X^" + str(i))
+        return polynomial
+
--- main.py
+++ main.py
@@ -6,5 +6,40 @@
 # This file generates random polynomials and runs the data collection
 
 
-if __name__ == '__main__':
+# Imports
+import random as ran
+import Polynomial
+import time
 
+
+
+# Functions
+
+# A function that returns a random polynomial array of a specified size with coefficients of +-99
+def randomArray(degree):
+    array = []
+    for i in range(degree+1):
+        array.append(ran.randint(-100, 100))
+    return array
+
+
+# A function that runs a set of 10 size points which are average multiplication time of 20
+# randomly generated polynomial multiplications, and prints it.
+def data():
+    points = [5, 10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000]
+    for point in points:
+        average = 0
+        for i in range(20):
+            poly1 = Polynomial(randomArray(i))
+            poly2 = Polynomial(randomArray(i))
+            start = time()
+            # Multiply poly1 and poly2 here
+            end = time()
+            average += end-start
+        print("The Average for degree " + str(point) + " was: " +str(average/20) + " ms")
+
+
+
+# Run Option
+if __name__ == '__main__':
+    data()
